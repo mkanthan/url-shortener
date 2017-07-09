073c6efe3fc871446df6fdb5878a864e03c0c714
Manu Kanthan <m.kanthan@gmail.com>
2017-07-09 13:12:23 -0400

    JavaScript is functional

--- app/assets/javascripts/create_link.js
+++ app/assets/javascripts/create_link.js
@@ -8,12 +8,23 @@ var CreateLink = {
       dataType: 'json',
       data: { link: { url } },
       success: function(link) {
+        $("#short-url-content").show();
+        $("#error-content").hide();
         $('p.shortened-url').html(link.url)
-        $('p.error').html("")
       }, error: function(data) {
-        $('p.shortened-url').html("")
+        $("#short-url-content").hide();
+        $("#error-content").show();
         $('p.error').html(data.responseJSON.errors.url)
       }
     });
+  },
+
+  copy: function() {
+    var proxyInput = document.createElement("input");
+    proxyInput.setAttribute("value", $('p.shortened-url').html());
+    document.body.appendChild(proxyInput);
+    proxyInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(proxyInput);
   }
 }
