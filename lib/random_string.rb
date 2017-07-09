9694d59e9b3cb3effded6ef64d901b3c9e0b5a7d
Manu Kanthan <m.kanthan@gmail.com>
2017-07-09 15:31:51 -0400

    Oops! Forgot to add numbers to the RandomString

--- lib/random_string.rb
+++ lib/random_string.rb
@@ -1,5 +1,5 @@
 class RandomString
-  AVAILABLE_CHARS = [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+  AVAILABLE_CHARS = [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten
 
   def self.generate(length)
     (0..length-1).map{ AVAILABLE_CHARS[rand(AVAILABLE_CHARS.length)] }.join
